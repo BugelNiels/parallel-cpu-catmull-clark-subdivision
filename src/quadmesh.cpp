4e4d361284a81271c2ca2419864813f249d56384
Albert <a.dijkstra.38@student.rug.nl>
2021-12-22 13:58:36 +0100

    started with optimizing face points

--- src/quadmesh.cpp
+++ src/quadmesh.cpp
@@ -18,10 +18,14 @@ void QuadMesh::insertFacePoints(QuadMesh& mesh) {
   for (int h = 0; h < numHalfEdges; h += 4) {
     // everything is a quad and these are stored contiguously in memory.
     // avoids the need for critical sections
-    facePoint(mesh, h, numVerts);
-    facePoint(mesh, h + 1, numVerts);
-    facePoint(mesh, h + 2, numVerts);
-    facePoint(mesh, h + 3, numVerts);
+      float m = cycleLength(h);
+      int i = numVerts + face(h);
+      QVector3D c = QVector3D(0,0,0);
+      for(int i; i < m; i++){
+          int v = vert(h);
+          c += vertexCoords[v];
+      }
+      mesh.vertexCoords[i] = c / m;
   }
 }
 
